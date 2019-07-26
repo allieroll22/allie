8bd29b50ab027b381ebdb3215c3e0f7ad1265085
“Allie <allieroll22@ufl.edu>
2019-07-26 12:36:19 -0400

    Edit post page

--- single.php
+++ single.php
@@ -7,7 +7,6 @@
         while(have_posts()){
           the_post();?>
           <h2 class="post-title"><?php the_title(); ?></h2>
-          <p class="post-date"><?php echo "Published: " . get_the_date(); ?></p>
           <div class="post-feature"> <?php the_post_thumbnail(); ?>
           </div>
             <?php the_content(); ?>
