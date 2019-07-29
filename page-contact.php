b4acaab5512e3e5bf2ead8e53ab662fc432c48d5
“Allie <allieroll22@ufl.edu>
2019-07-29 14:23:51 -0400

    Add contact template

--- page-contact.php
+++ page-contact.php
@@ -1,18 +1,9 @@
-<?php /*Template Name: About Page Template*/ ?>
+<?php /*Template Name: Contact Page Template*/ ?>
 
 <?php get_header(); ?>
 
 <div class="container">
-    <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-  <div class="row about">
-  <div class="eight columns">
-    <?php dynamic_sidebar('about-us'); ?>
-  </div>
-
-  <div class="four columns">
-    <?php dynamic_sidebar('about-us-image')?>
-  </div>
-</div>
+  <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 </div>
 
 
