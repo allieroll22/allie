870d1c0568bb9caedae8ab5fc09e924908146ea2
“Allie <allieroll22@ufl.edu>
2019-07-29 15:01:44 -0400

    Edit form

--- page-contact.php
+++ page-contact.php
@@ -5,6 +5,7 @@
 <div class="container">
   <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 </div>
+<?php [wpforms id="212" title="false" description="false"] ?>
 
 
 <?php get_footer(); ?>
