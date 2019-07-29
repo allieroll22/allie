0a18e687032ffff4e50e99658e3e1f1c9807a8c1
“Allie <allieroll22@ufl.edu>
2019-07-29 15:16:56 -0400

    Edit page form

--- page-contact.php
+++ page-contact.php
@@ -3,13 +3,13 @@
 <?php get_header(); ?>
 
 <div class="container">
+  <div class=""
   <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
   <?php
     if(have_posts()){
       while(have_posts()){
-        the_post();?>
-        <h2><?php the_title(); ?></h2>
-        <?php the_content();
+        the_post();
+        the_content();
       }//end while
     }//ends if
   ?>
