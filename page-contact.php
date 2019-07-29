e43dd48544782ca2abf69dad7e45ab3928d5219e
“Allie <allieroll22@ufl.edu>
2019-07-29 15:28:44 -0400

    Edit contact page

--- page-contact.php
+++ page-contact.php
@@ -3,7 +3,7 @@
 <?php get_header(); ?>
 
 <div class="container">
-  <div class=""
+  
   <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
   <?php
     if(have_posts()){
