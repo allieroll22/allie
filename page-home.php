aa704b651f0537017e3a1418665d76ffdb9158a6
“Allie <allieroll22@ufl.edu>
2019-07-29 18:03:29 -0400

    Edit homepage

--- page-home.php
+++ page-home.php
@@ -9,15 +9,7 @@
         </div>
      <?php } ?>
 
-<div class="container">
-  <!-- Hero Image Section -->
-  <div class="row">
-    <div class="twelve columns">
-      <?php dynamic_sidebar('hero-image'); ?>
-    </div>
-  </div>
 
-</div>
 
 
 <?php get_footer(); ?>
