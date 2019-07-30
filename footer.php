4d649c6d36b97a859eb652f58f336f7131a13374
“Allie <allieroll22@ufl.edu>
2019-07-30 17:51:37 -0400

    Edit footer

--- footer.php
+++ footer.php
@@ -2,13 +2,7 @@
   <div class="container footer">
     <div class="row">
 
-      <div class="two-thirds column">
-        <?php wp_nav_menu(array(
-          'theme-location' => 'footer-menu',
-          'container-class'=> 'menu-footer'
-        ));
-        ?>
-      </div>
+    
 
 
         <div class="one-third column">
