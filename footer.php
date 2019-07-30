33efb38f1d2e4e5db3e096772d3b8590431de2c1
“Allie <allieroll22@ufl.edu>
2019-07-30 17:41:54 -0400

    Edit footer

--- footer.php
+++ footer.php
@@ -2,17 +2,13 @@
   <div class="container footer">
     <div class="row">
 
-      <div class="one-third column">
+      <div class="two-thirds column">
         <?php wp_nav_menu(array(
           'theme-location' => 'footer-menu',
           'container-class'=> 'menu-footer'
         ));
         ?>
       </div>
-      <div class="one-third column">
-        <?php dynamic_sidebar('middle-footer'); ?>
-      </div>
-
 
 
         <div class="one-third column">
