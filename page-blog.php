58d55ac0cff8017e33a127e2b165e731621631bf
“Allie <allieroll22@ufl.edu>
2019-07-31 12:26:25 -0400

    Edit blog page posts

--- page-blog.php
+++ page-blog.php
@@ -4,20 +4,7 @@
 <div class="container">
   <div class="row">
     <div class="twelve columns single-content">
-      <?php
-        if(have_posts()){
-          while(have_posts()){
-            the_post();?>
-            <h2 class="post-title"><?php the_title(); ?></h2>
-            <div class="post-feature"> <?php the_post_thumbnail(); ?>
-            </div>
-            <div class="post-text">  <?php the_content(); ?> </div>
-
-
-        <?php  }//end while loop
-        } //end if statement
-
-      ?>
+    
 
       <?php
       $args = array('post_type'=>'post');
@@ -40,13 +27,6 @@
       ?>
 
 
-      <?php global $post; // required
-      $args = array('category' => -9); // exclude category 9
-      $custom_posts = get_posts($args);
-      foreach($custom_posts as $post) : setup_postdata($post);
-      ...
-      endforeach;
-      ?>
   </div>
 </div>
 </div>
