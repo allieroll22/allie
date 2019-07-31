557aa27de9b31ef294f6688f24cf24f5e4d01ee4
“Allie <allieroll22@ufl.edu>
2019-07-31 12:25:10 -0400

    Edit blog page

--- page-blog.php
+++ page-blog.php
@@ -19,6 +19,35 @@
 
       ?>
 
+      <?php
+      $args = array('post_type'=>'post');
+      // The Query
+      $the_query = new WP_Query( $args );
+
+      // The Loop
+      if ( $the_query->have_posts() ) {
+          echo '<ul>';
+          while ( $the_query->have_posts() ) {
+              $the_query->the_post();
+              echo '<li>' . get_the_title() . '</li>';
+          }
+          echo '</ul>';
+      } else {
+          // no posts found
+      }
+      /* Restore original Post Data */
+      wp_reset_postdata();
+      ?>
+
+
+      <?php global $post; // required
+      $args = array('category' => -9); // exclude category 9
+      $custom_posts = get_posts($args);
+      foreach($custom_posts as $post) : setup_postdata($post);
+      ...
+      endforeach;
+      ?>
   </div>
+</div>
 </div>
 <?php get_footer(); ?>
