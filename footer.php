ba72b36895fbd90d774c261fdc180b38acb19c52
“Allie <allieroll22@ufl.edu>
2019-08-01 12:23:21 -0400

    Edit footer

--- footer.php
+++ footer.php
@@ -2,16 +2,16 @@
   <div class="container footer">
     <div class="twelve columns">
 
-      <div class="footer-middle">
-        <h4> Contact me </h4>
+      <div class="footer-right">
+        <p> Contact me </p>
         <p> allieroll22@gmail.com</p>
       </div>
 
-
+      <div class="one column">
 
           <?php dynamic_sidebar('right-footer'); ?>
 
-
+      </div>
 
     </div>
 
