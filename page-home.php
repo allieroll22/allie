f5d66d250c97dbd2213f5dfd5e679c3ef7c28204
“Allie <allieroll22@ufl.edu>
2019-12-08 16:31:17 -0500

    Edit homepage

--- page-home.php
+++ page-home.php
@@ -11,10 +11,10 @@
      <div class="hero-image-text">
        <h1 class="hi-text">
          Hi, I'm Allie.</h1>
-
+         <?php dynamic_sidebar('about-us'); ?>
     </div>
     <div class="container about">
-        <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
       <div class="row about">
       <div class="eight columns">
         <?php dynamic_sidebar('about-us'); ?>
