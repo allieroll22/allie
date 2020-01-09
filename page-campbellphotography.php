7243a1fa746623e19b8bac1e56da9479620c11da
“Allie <allieroll22@ufl.edu>
2020-01-09 15:39:23 -0500

    Add page

---
+++ page-campbellphotography.php
@@ -0,0 +1,31 @@
+<?php /*Template Name: Sub Projects Page Template*/ ?>
+
+<?php get_header(); ?>
+
+<div class="container sub-projects">
+    <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+    <div class="header-projects">
+      <?php dynamic_sidebar('project-header-text'); ?>
+
+      <?php dynamic_sidebar('project-header'); ?>
+    </div>
+
+    <div class="row projects">
+    <div class="seven columns">
+      <?php dynamic_sidebar('project-text'); ?>
+    </div>
+
+    <div class="five columns">
+      <?php dynamic_sidebar('project-image')?>
+    </div>
+    </div>
+
+    <div class="back-projects">
+      <?php dynamic_sidebar('project-link')?>
+    </div>
+
+
+</div>
+
+<?php get_footer(); ?>
