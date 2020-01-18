89d0e206f095d1fa7e0426993a4761c361904a89
“Allie <allieroll22@ufl.edu>
2020-01-18 12:31:04 -0500

    Edit about columns

--- page-about.php
+++ page-about.php
@@ -5,7 +5,7 @@
 <div class="container about">
     <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
   <div class="row about">
-  <div class="seven columns">
+  <div class="seven columns about">
     <?php dynamic_sidebar('about-us'); ?>
   </div>
 
