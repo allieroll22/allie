b831bb481f7d2c47c2aff29b3afa404a4c7b1d18
“Allie <allieroll22@ufl.edu>
2020-01-31 16:34:57 -0500

    Add div to close row for moni

--- page-monibasu.php
+++ page-monibasu.php
@@ -22,10 +22,12 @@
     <div class="five columns projects">
       <?php dynamic_sidebar('moni-image')?>
     </div>
-    </div>
     <div class="twelve columns button-area">
       <a href="http://www.allieroll.com/projects"><button class="projects">Back to Projects</button></a>
     </div>
+
+  </div>
+
 
     <?php dynamic_sidebar('moni-gallery'); ?>
 
