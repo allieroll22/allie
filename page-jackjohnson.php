6de89296542203e6949299a79eba0c170be5e709
“Allie <allieroll22@ufl.edu>
2020-01-31 14:23:00 -0500

    Edit page

--- page-jackjohnson.php
+++ page-jackjohnson.php
@@ -12,15 +12,12 @@
     </div>
 
     <div class="row projects">
-    <div class="seven columns">
+    <div class="twelve columns">
       <?php dynamic_sidebar('jackj-text'); ?>
           <a href="http://www.allieroll.com/jack-johnson/" target="_blank" rel="noopener"><button class="website">View the Website</button></a>
     </div>
 
-    <div class="five columns projects">
-      <?php dynamic_sidebar('jackj-image')?>
-    </div>
-    </div>
+  
 
     <?php dynamic_sidebar('jackj-gallery'); ?>
 
