84dff254396f5fabd17bad31bffdbc37c8fb4ec0
“Allie <allieroll22@ufl.edu>
2020-02-06 23:03:17 -0500

    Add favicon to header

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <!--favicon-->
+    <link rel="shortcut icon" href="favicon.ico">
 
     <title><?php bloginfo('name'); ?></title>
 
